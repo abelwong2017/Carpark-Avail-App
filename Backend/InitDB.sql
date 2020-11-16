5512b5baf161cf4bdd56112766cd16f6b5ab76ff
abelwong2017 <abel.wong.2017@sis.smu.edu.sg>
2020-11-16 18:35:59 +0800

    Add SQL Script

---
+++ Backend/InitDB.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS carparkDB;
+CREATE SCHEMA IF NOT EXISTS carparkDB;
+use carparkDB;
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    contact_number BIGINT(50),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
